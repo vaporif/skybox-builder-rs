03c37299712b36485031510581bcdbbcd6c736f0
Dmitriy Onypko <vaporif@gmail.com>
2022-07-14 23:50:01 +0100

    upd

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,39 @@
-use std::fs;
+use std::{fs, io::Error, path::{PathBuf, Path}};
 
 use image::DynamicImage;
 
-pub fn merge_all_files(path: &str) {
-    for file in fs::read_dir(path).expect("wrong path") {
-        println!("{}", file.unwrap().path().display());
+pub fn merge_all_files(path: &str) -> Result<(), Error> {
+    let file_paths = get_file_paths(path)?;
+    let skyboxes = get_skyboxes(file_paths);
+    
+    for skybox in skyboxes.iter().map(|f| SkyBoxImageGroup::new(f)) {
+        skybox.merge();
     }
 
-    let img = image::open("tests/images/jpg/progressive/cat.jpg").unwrap();
+    Ok(())
 }
 
-struct SkyBoxFilePathGroup<'a> {
+fn get_file_paths(mut dir_path: &str) -> Result<Vec<PathBuf>, Error> {
+    if dir_path.is_empty() {
+        dir_path = &"./";
+    }
+
+    let paths = fs::read_dir(dir_path)?;
+    
+    Ok(paths.into_iter().map(|f| f.unwrap().path()).collect())
+}
+
+
+fn get_skyboxes(paths: Vec<PathBuf>) -> Vec<SkyBoxFilePath<'static>> {
+    unimplemented!()
+}
+
+struct SkyBoxFilePath<'a> {
     left_side: &'a str,
     center: &'a str,
     right_side: &'a str,
     top_side: &'a str,
-    bottom_side: &'a str
+    bottom_side: &'a str,
 }
 
 struct SkyBoxImageGroup {
@@ -23,11 +41,11 @@ struct SkyBoxImageGroup {
     center: DynamicImage,
     right_side: DynamicImage,
     top_side: DynamicImage,
-    bottom_side: DynamicImage
+    bottom_side: DynamicImage,
 }
 
 impl SkyBoxImageGroup {
-    fn new(file_group: SkyBoxFilePathGroup) -> SkyBoxImageGroup {
+    fn new(file_group: &SkyBoxFilePath) -> SkyBoxImageGroup {
         Self {
             left_side: image::open(file_group.left_side).unwrap(),
             center: image::open(file_group.center).unwrap(),
@@ -35,5 +53,19 @@ impl SkyBoxImageGroup {
             top_side: image::open(file_group.top_side).unwrap(),
             bottom_side: image::open(file_group.bottom_side).unwrap(),
         }
+    }
+
+    fn merge(self) {
+
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn get_skyboxes_group() {
+       
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
-use std::env;
+use std::{env, io::Error};
 
 use skybox_composer::merge_all_files;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    match args.len() {
-        0 => merge_all_files(""), 
-        1 => merge_all_files(&args[0]),
-        _ => panic!("Either enter images to merge path or run without args")
+    if let Err(e) = run() {
+        panic!("{}", e);
     }
 }
 
+fn run() -> Result<(), Error> {
+    let args: Vec<String> = env::args().collect();
+    match args.len() {
+        0 => merge_all_files("")?,
+        1 => merge_all_files(&args[0])?,
+        _ => panic!("Either enter images to merge path or run without args"),
+    }
 
+    Ok(())
+}
