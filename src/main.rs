cd017d974cb8dc26cc4f6109b8acbc2d6437f07a
Dmitriy Onypko <vaporif@gmail.com>
2023-03-26 15:34:20 +0100

    upd

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ fn main() {
 fn run() -> Result<(), Error> {
     let delete_flag = "delete";
     let matches = command!()
-        .version("0.2.0")
+        .version("0.2.1")
         .author("Dmytro O. <vaporif@gmail.com>")
         .about("Skybox file merger")
         .arg(
